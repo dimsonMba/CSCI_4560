dbebd06e85027535263fe2934c5e25717fbdebee
dimsonMba <dimitrisinou@gmail.com>
2025-02-06 21:19:25 -0600

    add the logo on the page, link the first page and sign up page

--- my-app/src/assets/components/Sign_in/SignIn.jsx
+++ my-app/src/assets/components/Sign_in/SignIn.jsx
@@ -6,13 +6,13 @@ const SignIn = () => {
     <> 
       <div className='split_Sign left_Sign'>
         <div >
-          <img src='./icon_comments_chat.png' alt="Chat Icon" />
+          <img src= "./chat_icon.jpg" alt="Chat Icon" />
           <h1 className='slogan'>Keep your friends and network closer</h1></div>
         <div className='app_name centered'>UniConnect</div>
       </div>
 
-      <div className='split_sign right_sign'>
-        
+      <div className='split_Sign right_Sign'>
+        <h1>food</h1>
       </div>
     </>
     
